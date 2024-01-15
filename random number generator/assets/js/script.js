660d28a88d64fc5cb938bd930c063f96275430fe
SiphoPrinceMarubelela <siphoprincemarubelela@gmail.com>
2024-01-16 00:31:04 +0200

    uploaded functionality to mimic 3 die rolled @ once

--- random number generator/assets/js/script.js
+++ random number generator/assets/js/script.js
@@ -1,13 +1,22 @@
 const btnRoll = document.getElementById("myButton");
-const lblRoll = document.getElementById("myLabel");
+const lblRoll1 = document.getElementById("label1");
+const lblRoll2 = document.getElementById("label2");
+const lblRoll3 = document.getElementById("label3");
 
-//this random number generator will mimic rolling a 6 sided dice:
+//this random number generator will mimic rolling a 6 sided dice (now 3 times...):
 const min = 1;
 const max = 6;
 
-let randomNumber;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
 
 btnRoll.onclick = function() {
-  randomNumber = Math.floor(Math.random() * max) + min;
-  lblRoll.textContent = randomNumber;
+  randomNumber1 = Math.floor(Math.random() * max) + min;
+  randomNumber2 = Math.floor(Math.random() * max) + min;
+  randomNumber3 = Math.floor(Math.random() * max) + min;
+
+  lblRoll1.textContent = randomNumber1;
+  lblRoll2.textContent = randomNumber2;
+  lblRoll3.textContent = randomNumber3;
 }
